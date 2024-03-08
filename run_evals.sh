e58208736e97ac1a22c14c62258e7d3756ddf625
Evžen Wybitul <wybitul.evzen@gmail.com>
2024-03-08 17:58:37 +0000

    Modify eval script to use the new file

--- run_evals.sh
+++ run_evals.sh
@@ -2,13 +2,13 @@
 
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --time=2:00:00
+#SBATCH --time=6:00:00
 #SBATCH --partition=single
 #SBATCH --job-name=vlmrm
 #SBATCH --output=logs/job_%j.log
 
-models=("clip" "viclip" "s3d" "gpt4")
+models=("gpt4")
 
 for model in "${models[@]}"; do
-    python src/evaluation/evaluator.py -t data/evaluation/data.csv -m "$model" -r logit,projection -a 0.0,0.25,0.50,0.75,1.0 -n 32 -e standardized_improved --standardize
+    python src/evaluation/multiclass_evaluator.py -d data/habitat/data.csv -t data/habitat/tasks.yaml -m "$model" -r logit,projection -a 0.0,0.25,0.50,0.75,1.0 -n 32 -e gpt4_test
 done
